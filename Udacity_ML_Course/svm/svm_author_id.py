305c4df1d2727b58759d588c4ced5510ae7b8ffd
shibby576 <jhirko12@gmail.com>
2018-12-13 14:38:48 -0500

    Wrote an SVM to classify emails by author!

--- Udacity_ML_Course/svm/svm_author_id.py
+++ Udacity_ML_Course/svm/svm_author_id.py
@@ -22,9 +22,42 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
-#########################################################
-### your code goes here ###
 
-#########################################################
+from sklearn import svm
+from time import time
+
+#reduce the trianing data
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
 
 
+clf = svm.SVC(kernel='rbf', C=10000)
+t0 = time()
+clf.fit(features_train, labels_train)
+#print trianing time
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+
+#get individual predictions
+ten = pred[10]
+two_six = pred[26]
+fifty = pred[50]
+
+#Get number that are predicted to be chris (1)
+chris = pred.sum()
+
+#print prediction  time
+print "prediction time:", round(time()-t0, 3), "s"
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+
+print('Accuracy ' + str(acc))
+
+#print individual predictions
+print('#10 ' + str(ten) + ' #26 ' + str(two_six) + ' #50 ' + str(fifty))
+
+#print number that are chris
+print(chris)
