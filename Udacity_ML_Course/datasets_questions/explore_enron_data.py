ccdcd6063e13473537f31af44dc778c21c735e62
shibby576 <jhirko12@gmail.com>
2018-12-18 12:38:57 -0500

    EXPLORING Enron data as part of data exploration section in class

--- Udacity_ML_Course/datasets_questions/explore_enron_data.py
+++ Udacity_ML_Course/datasets_questions/explore_enron_data.py
@@ -17,13 +17,12 @@
 """
 
 import pickle
-import numpy as np
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
-enron_array = np.array(enron_data)
 
 #number of data points
-print (len(enron_data))
+total_data_points = len(enron_data)
+print (total_data_points)
 
 #number of features per person
 print (len(enron_data.values()[1]))
@@ -34,16 +33,56 @@ for i in enron_data:
     if enron_data[i]['poi']==1:
         counter +=1
 
+print "Number of POIs in data set: ", str(counter)
 
+#get the total number of POIs from poi names file
+poi_names = open("/Users/jonathan/Documents/GitHub/data_sandbox/Udacity_ML_Course/final_project/poi_names.txt", "r")
+counter_1 = 0
+for i in poi_names:
+    counter_1 +=1
+print "Total POIs: ", str(counter_1)
 
+#get an individuals stock value
 
+print("James Prentice total stock value " + str(enron_data["PRENTICE JAMES"]["total_stock_value"]))
+#print(enron_data.values())
 
+#number of messages fromWesley Colwell
+print "Messages from Wesley Colwell TO POIs " + str(enron_data["COLWELL WESLEY"]["from_this_person_to_poi"])
 
+#print(enron_data)
+#value of stock options exercised by Jeffrey K Skilling
+print "Value of stock options exercised by Jeffrey K Skilling " + str(enron_data['SKILLING JEFFREY K']['exercised_stock_options'])
 
+#total paymets for top enron employees
+print "Total payments Jeffrey K Skilling " + str(enron_data['SKILLING JEFFREY K']['total_payments'])
+print "Total payments LAY KENNETH L " + str(enron_data['LAY KENNETH L']['total_payments'])
+print "Total payments FASTOW ANDREW S " + str(enron_data['FASTOW ANDREW S']['total_payments'])
 
+#number of people with a salary
+counter =0
+for i in enron_data:
+    if enron_data[i]['salary']!= 'NaN':
+        counter +=1
 
+print "Number of missing salaries: ", str(counter)
 
-print("Total number of POIs: ", str(counter))
+#number of people with an email address
+counter =0
+for i in enron_data:
+    if enron_data[i]['email_address']!= 'NaN':
+        counter +=1
 
+print "Number of missing email addresses: ", str(counter)
 
+#how many people have NaN for total payments?
+nan_counter =0.0
+for i in enron_data:
+    if enron_data[i]['total_payments']== 'NaN':
+        nan_counter +=1
 
+print "Number of missing total payments: ", str(nan_counter)
+
+#percent of people with missing total payments
+perct = nan_counter/total_data_points
+print('% of missing total payments ' + str(perct))
