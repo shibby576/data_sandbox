7ae5eb1d5970075f47c6ffc5e8725e46cde63915
shibby576 <jhirko12@gmail.com>
2018-12-19 11:12:49 -0500

    Regression module for Udacity course. Ran regressions on enron data to predict bonus amount

--- Udacity_ML_Course/regression/finance_regression.py
+++ Udacity_ML_Course/regression/finance_regression.py
@@ -22,14 +22,14 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
-data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
+data = featureFormat( dictionary, features_list, remove_any_zeroes=True, sort_keys = '../tools/python2_lesson06_keys.pkl')
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,7 +38,11 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
-
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression().fit(feature_train,target_train)
+print('r squared: ' + str(reg.score(feature_test,target_test)))
+print('slope: ' + str(reg.coef_))
+print('intercept: ' + str(reg.intercept_))
 
 
 
@@ -64,6 +68,10 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+print(reg.coef_)
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
