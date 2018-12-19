a854ec625c10763c4e57e643e9147105ae5103c5
shibby576 <jhirko12@gmail.com>
2018-12-19 13:21:24 -0500

    Working on removing outliers in data

--- Udacity_ML_Course/outliers/enron_outliers.py
+++ Udacity_ML_Course/outliers/enron_outliers.py
@@ -10,10 +10,25 @@ from feature_format import featureFormat, targetFeatureSplit
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
+data_dict.pop('TOTAL', 0)
 data = featureFormat(data_dict, features)
 
+data.max
+mil = []
+bon = []
+for i in data_dict:
+    if (data_dict[i]['salary'] >= 1000000 and data_dict[i]['salary'] != 'NaN') and (data_dict[i]['bonus'] >= 5000000 and data_dict[i]['bonus'] != 'NaN'):
+        bon.append(i)
 
-### your code below
+print(bon)
 
 
 
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
+
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
