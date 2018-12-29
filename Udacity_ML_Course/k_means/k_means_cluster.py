fc6b6b7ccd3c6da49cde00db21535a2963276e37
shibby576 <jhirko12@gmail.com>
2018-12-29 12:06:48 -0500

    SKLearn Clustering and K-means

--- Udacity_ML_Course/k_means/k_means_cluster.py
+++ Udacity_ML_Course/k_means/k_means_cluster.py
@@ -48,6 +48,7 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
 features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
@@ -59,18 +60,38 @@ poi, finance_features = targetFeatureSplit( data )
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
 for f1, f2 in finance_features:
-    plt.scatter( f1, f2 )
+    plt.scatter( f1, f2)
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-
+from sklearn.cluster import KMeans
+kmeans = KMeans(n_clusters=2).fit(finance_features)
+pred = kmeans.predict(finance_features)
 
 
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters_1.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
+
+
+#find the range of exercised options
+options = []
+for i in data_dict.values():
+    if i['exercised_stock_options'] != 'NaN':
+        options.append(i['exercised_stock_options'])
+print('max options: ' + str(max(options)))
+print('min options: ' + str(min(options)))
+
+#find the range of salary
+salary = []
+for i in data_dict.values():
+    if i['salary'] != 'NaN':
+        salary.append(i['salary'])
+print('max salary: ' + str(max(salary)))
+print('min salary: ' + str(min(salary)))
+
